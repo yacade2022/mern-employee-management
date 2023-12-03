5a02a7a4265cbd2d1cbf5b882b74427c6196a7df
yacade2022 <amyac824@gmail.com>
2023-12-03 16:13:42 +0100

    updatd

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ import {authenticationUser} from  './middleware/authenticationUser.js'
 import errorHandlerMiddlware from './middleware/errorHandlerMiddleware.js'
 import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
+import rateLimiter from 'express-rate-limit'
+import helmet from 'helmet'
+import xss from 'xss-clean'
+import cors from 'cors'
+import mongoSanitize from 'express-mongo-sanitize'
+
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 if(process.env.NODE_ENV === 'development'){
@@ -23,6 +29,17 @@ if(process.env.NODE_ENV === 'development'){
 
 app.use(express.static(path.resolve(__dirname, './public')));
 
+app.set('trust proxy', 1)
+app.use(
+    rateLimiter({
+        windowMs: 20 * 60 * 1000,
+        max:60
+    })
+)
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(mongoSanitize())
 app.use(cookieParser())
 app.use(express.json())
 
